adbd691b0531b28eccc974902bc02c4a35dde408
michael emmons <memmons18@gmail.com>
2020-06-04 11:46:20 -0400

    information added about week and homework assignment is saved in a text file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const githubReadme = require("./questions/github.js");
 const readmeCreation = require("./text-file-creation/readme.js");
-const gradeQuestions = require("./questions/grading.js")
+const gradeQuestions = require("./questions/grading.js");
 let readmeAnswers;
 //global variables for assignment and week grading part of app is working with
 let assignment;
@@ -65,68 +65,27 @@ function updateReadMeAnswers(url, credit, license) {
   readmeAnswers.creditInput = credit;
   readmeAnswers.licenseInput = license;
 
-  saveFile(readmeCreation(readmeAnswers));
+  saveFile("./readme_created/README.md", readmeCreation(readmeAnswers));
 }
 
-//add data to the readme file
-function saveFile(data) {
-  fs.writeFile("./readme_created/README.md", data, function (err) {
+//add data to file
+function saveFile(filepath, data) {
+  fs.writeFile(filepath, data, function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log("readme created successfully");
+      console.log("file created successfully in " + filepath);
     }
   });
 }
 
-
-fs.readFileSync("./readme_created/readme.md", "utf-8", function (r, j) {
-  console.log(j)
-})
+// fs.readFileSync("./readme_created/readme.md", "utf-8", function (r, j) {
+//   console.log(j)
+// })
 
 //separate function from the readme creator
 
-//get information about the week and assignment
-function studentGradeCreate() {
-  console.log("start student grade app");
-
-
-  inquirer.prompt(gradeQuestions.assignmentInput).then(function (res) {
-
-    assignment = res.assignment_name
-    week = res.week_num
-
-    addStudentInfo()
-
-  })
-
-  //update information about the week and assignment with student information (name, grade, comments). Place it into a constructor and push it to an object to use later
-  function addStudentInfo() {
-    inquirer.prompt(gradeQuestions.gradeInput).then(function (res) {
-      let x = new submission(assignment, week, res.student_name, res.grade, res.comments)
-
-      gradeQuestions.results.push(x)
-      console.log(gradeQuestions.results)
-      checkForMore()
-    })
-
-  }
-
-  //check if more students need to be added
-  function checkForMore() {
-    inquirer.prompt(gradeQuestions.addMore).then(function (res) {
-      if (res.add_more) {
-        addStudentInfo()
-      }
-      else {
-        console.log("thank you we will now send information to a text file")
-      }
-    })
-
-  }
-
-
-}
+//**** student grading section of program ****/
 
 //constructor for building object about student
 function submission(assignment, week, name, grade, comments) {
@@ -134,5 +93,49 @@ function submission(assignment, week, name, grade, comments) {
   this.week = week;
   this.name = name;
   this.grade = grade;
-  this.comments = comments
+  this.comments = comments;
+}
+
+//get information about the week and assignment
+function studentGradeCreate() {
+  console.log("start student grade app");
+
+  inquirer.prompt(gradeQuestions.assignmentInput).then(function (res) {
+    assignment = res.assignment_name;
+    week = res.week_num;
+
+    addStudentInfo();
+  });
+
+  //update information about the week and assignment with student information (name, grade, comments). Place it into a constructor and push it to an object to use later
+  function addStudentInfo() {
+    inquirer.prompt(gradeQuestions.gradeInput).then(function (res) {
+      let x = new submission(
+        assignment,
+        week,
+        res.student_name,
+        res.grade,
+        res.comments
+      );
+
+      gradeQuestions.results.push(x);
+      console.log(gradeQuestions.results);
+      checkForMore();
+    });
+  }
+
+  //check if more students need to be added
+  function checkForMore() {
+    inquirer.prompt(gradeQuestions.addMore).then(function (res) {
+      if (res.add_more) {
+        addStudentInfo();
+      } else {
+        console.log("thank you we will now send information to a text file");
+        saveFile(
+          "./readme_created/week" + week + ".txt",
+          JSON.stringify(gradeQuestions.results)
+        );
+      }
+    });
+  }
 }
